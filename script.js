49688d94b2d47b979a53e3c7ee5f6e0e73da5ea1
fatimazohrasouci-48 <fatimazohrasouci@gmail.com>
2026-05-17 17:56:00 +0200

    update

--- script.js
+++ script.js
@@ -596,3 +596,23 @@ new Chart(ctx, {
   }
 
 });
+/* =========================
+   FAQ INTERACTIVE
+========================= */
+
+const faqQuestions =
+document.querySelectorAll(".faq-question");
+
+
+faqQuestions.forEach(function(question){
+
+  question.addEventListener("click", function(){
+
+    const faqItem =
+      this.parentElement;
+
+    faqItem.classList.toggle("active");
+
+  });
+
+});
